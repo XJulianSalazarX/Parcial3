8b2f225f93c37eeca08efae03d968eec5760e15c
julian salazar <julian.salazard@udea.edu.co>
2021-02-04 21:22:28 -0500

    interfaz grafica

--- widget.cpp
+++ widget.cpp
@@ -6,6 +6,14 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    this->setMinimumSize(width(),height());
+    this->setMaximumSize(width(),height());
+    scene = new QGraphicsScene();
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->setFixedSize(width(),height()-20);
+    ui->graphicsView->setSceneRect(0,0,width(),height()-20);
+    ui->graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 }
 
 Widget::~Widget()
